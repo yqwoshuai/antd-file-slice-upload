73d6474a84dd10036c578abe13f8bf7190affc26
yqwoshuai <358805791@qq.com>
2021-05-17 18:05:48 +0800

    基础分片请求

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,135 @@
 import React, { useState } from "react";
-import { Button, Upload } from "antd";
+import { Button, Upload, Table, Progress, Row, Col, Space } from "antd";
+import { loadLimit } from "./utils";
 import axios from "axios";
 import "./index.css";
 
 // 切片大小 1M
-const FILE_CHUNK_SIZE = 1 * 1024 * 1024;
+const FILE_CHUNK_SIZE = 2 * 1024 * 1024;
 
 // 切片文件
 const createFileChunk = (file, size = FILE_CHUNK_SIZE) => {
-	const fileChunkList = [];
-	let cur = 0;
-	while (cur < file.size) {
-		fileChunkList.push({ file: file.slice(cur, cur + size), name: file.name });
-		cur += size;
-	}
-	return fileChunkList;
+  const fileChunkList = [];
+  let cur = 0;
+  while (cur < file.size) {
+    fileChunkList.push({ file: file.slice(cur, cur + size), name: file.name });
+    cur += size;
+  }
+  return fileChunkList;
 };
 
+const columns = [
+  {
+    title: "切片名称",
+    dataIndex: "name",
+  },
+  {
+    title: "切片大小",
+    dataIndex: "size",
+    width: 150,
+  },
+  {
+    title: "加载进度",
+    dataIndex: "percent",
+    width: 550,
+    render: (row) => {
+      return <Progress percent={row} />;
+    },
+  },
+];
+
 function App() {
-	const [nowFile, setNowFile] = useState(null);
-	const [fileList, setFileList] = useState([]);
+  const [nowFile, setNowFile] = useState(null);
+  const [fileList, setFileList] = useState([]);
 
-	const handleFileChange = (e) => {
-		if (!e.fileList[0]) return;
-	};
+  const beforeUpload = (e) => {
+    setNowFile(e);
+    const chunkList = createFileChunk(e).map(({ file, name }, index) => {
+      return {
+        chunk: file,
+        size: file.size,
+        percent: 0,
+        name: name + " -" + (index + 1),
+        index,
+      };
+    });
+    setFileList(chunkList);
+    return false;
+  };
 
-	const beforeUpload = (e) => {
-		const chunkList = createFileChunk(e).map(({ file, name }, index) => {
-			return {
-				chunk: file,
-				size: file.size,
-				percent: 0,
-				name: name + " -" + (index + 1),
-			};
-		});
-		setFileList(chunkList);
-	};
+  const handleUpload = async (e) => {
+    const requestList = fileList
+      .map(({ chunk, index }) => {
+        const formData = new FormData();
+        formData.append("chunk", chunk);
+        formData.append("filename", nowFile.name);
+        return { formData, index };
+      })
+      .map(async ({ formData, index }) => {
+        return axios({
+          url: "http://localhost:3001/",
+          method: "post",
+          data: formData,
+          onUploadProgress: (e) => {
+            const newList = fileList.map((item) => {
+              if (item.index === index) {
+                item.percent = ((e.loaded / e.total) * 100).toFixed(2);
+              }
+              return item;
+            });
+            setFileList(newList);
+          },
+        });
+      });
+    await Promise.all(requestList);
+  };
 
-	const handleUpload = (e) => {
-		const data = fileList.map(({ chunk }, index) => {
-			const formData = new FormData();
-			formData.append("chunk", chunk);
-			formData.append("filename", e.file.name + " -" + (index + 1));
-			return { formData, index };
-		});
+  const props = {
+    showUploadList: false,
+    beforeUpload: beforeUpload,
+  };
 
-		const requestList = data.map(({ formData, index }) => {
-			return axios({
-				url: "http://localhost:3001/",
-				method: "post",
-				data: formData,
-			});
-		});
-
-		Promise.all(requestList);
-	};
-
-	const props = {
-		action: "http://localhost:3001/",
-		progress: {
-			strokeColor: {
-				"0%": "#108ee9",
-				"100%": "#87d068",
-			},
-			strokeWidth: 3,
-			format: (percent) => `${parseFloat(percent.toFixed(2))}%`,
-		},
-		onChange: handleFileChange,
-		customRequest: handleUpload,
-		beforeUpload: beforeUpload,
-		fileList,
-	};
-
-	return (
-		<div className="app">
-			<Upload {...props}>
-				<Button>点击上传</Button>
-			</Upload>
-		</div>
-	);
+  return (
+    <div className="app">
+      <Space direction="vertical" size="middle">
+        <Row justify={"space-between"} align={"middle"}>
+          <Col>
+            <Upload {...props}>
+              <Button>选择文件</Button>
+            </Upload>
+          </Col>
+          {nowFile && <Col span={18}>{nowFile.name}</Col>}
+          <Col>
+            <Button type="primary" onClick={handleUpload}>
+              确认上传
+            </Button>
+          </Col>
+        </Row>
+        {nowFile && (
+          <div>
+            <span>总进度：</span>
+            <Progress percent={0} />
+          </div>
+        )}
+        {fileList.length > 0 && (
+          <Table
+            rowKey={"name"}
+            columns={columns}
+            dataSource={fileList}
+            locale={{ emptyText: "请选择文件" }}
+            pagination={false}
+          ></Table>
+        )}
+      </Space>
+    </div>
+  );
 }
 
 export default App;
+
+const handleUpload = () => {
+  const formData = new FormData();
+  fileList.forEach((file) => {
+    formData.append("file", file);
+  });
+};
